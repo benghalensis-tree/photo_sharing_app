8455205c4f6e8e5be4af3d2e67bfb3bcd165de6d
genki utsumi <zabanya0770@gmail.com>
2023-05-30 00:05:31 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
+  resources :sessions, only:[:create, :destroy, :new]
   resources :users, only: [:new, :create, :show]
   resources :feeds do
     collection do
