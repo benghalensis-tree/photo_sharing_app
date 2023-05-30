21f9e651ceafb98b5a22d592c3283072a4a62a1a
genki utsumi <zabanya0770@gmail.com>
2023-05-30 17:01:06 +0900

    お気に入り機能の実装(仮）

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
+  resources :favorites, only: [:create, :destroy]
   resources :sessions, only:[:create, :destroy, :new]
   resources :users, only: [:new, :create, :show]
   resources :feeds do
