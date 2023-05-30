7554fea8d9311dc32a40f21dc617a41050131632
genki utsumi <zabanya0770@gmail.com>
2023-05-30 20:53:37 +0900

    ユーザー編集機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :favorites, only: [:create, :destroy, :index]
   resources :sessions, only:[:create, :destroy, :new]
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :feeds do
     collection do
       post :confirm
